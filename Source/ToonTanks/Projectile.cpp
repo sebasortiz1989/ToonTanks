d2ea802f9351c7b5a67588e1c6ce5453fdbaa382
Juan <juan@sdsharp.com>
2021-11-21 14:28:19 -0500

    37. Smoke Trail

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -4,6 +4,7 @@
 #include "Projectile.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystemComponent.h"
 
 // Sets default values
 AProjectile::AProjectile()
@@ -17,6 +18,9 @@ AProjectile::AProjectile()
 	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement Component"));
 	ProjectileMovementComponent->MaxSpeed = 1300.f;
 	ProjectileMovementComponent->InitialSpeed = 1300.f;
+
+	SmokeTrailParticle = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Smoke Trail Particles"));
+	SmokeTrailParticle->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
--- Source/ToonTanks/Projectile.h
+++ Source/ToonTanks/Projectile.h
@@ -35,6 +35,9 @@ private:
 	UPROPERTY(EditAnywhere, Category = "Combat")
 	class UParticleSystem* HitParticles;
 
+	UPROPERTY(VisibleAnywhere, Category = "Combat")
+	class UParticleSystemComponent* SmokeTrailParticle;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
